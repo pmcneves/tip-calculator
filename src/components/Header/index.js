c7077df789eb463a5519b9601c311c6453794050
PedroNeves <pneves355@gmail.com>
2021-07-26 11:09:23 +0100

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="logo">SPLI</h1>
+            <h1 className="logo">TTER</h1>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/screens/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Tips from "../containers/Tips";
+
+const TipCalculator = () => {
+  return (
+    <div>
+      <Header/>
+      <Tips />
+    </div>
+  );
+}
+
+export default TipCalculator;
